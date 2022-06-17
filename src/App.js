43f7ab10da2a1fe3c150116fa8a8f47895c83f07
NAZIR HUSSAIN <56648155+nazir-hussain@users.noreply.github.com>
2022-06-17 19:49:23 +0530

    committs

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
         <li className="nav-item">
           <a className="nav-link" href="/">About</a>
         </li>
-        
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
