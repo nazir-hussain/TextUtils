03a82ba84ed1e725cc0155d6ce9790457fa72af6
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-06-21 12:32:25 +0530

    Update TextForms.js

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -23,7 +23,8 @@ export default function TextForms(props) {
     // text ="new text";  //wrong way to change the state
     // setText ("new text");  //correct way to change the state
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="6"></textarea>
@@ -31,5 +32,11 @@ export default function TextForms(props) {
             <button className=" btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className=" btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
         </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} charecters</p>
+            <p>{0.008* text.split(" ").length}Reading time</p>
+        </div>
+        </>
     );
 }
