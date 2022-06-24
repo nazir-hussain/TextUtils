8374fa7b935d8018762eb7845a8b7b19cb363325
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-06-24 23:46:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import TextForms from './components/TextForms';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+ 
   return (
     <>
-      <Navbar title="TextUtils" about="About-Us" />
-      {/* <Navbar/> */}
-      <div className="container my-3">
-        
-    
-      <TextForms heading = "Enter the Text to Analyze below:" />
-      </div>
-    </>
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Switch>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          <Route exact path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+          </Route>
+    </Switch>
+    </div>
+    </Router>
+    </> 
   );
 }
 
